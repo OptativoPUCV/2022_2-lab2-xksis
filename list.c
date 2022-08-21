99fbb54284086c3fc9d44338e73b6a18fea9bb75
Replit user <>
2022-08-21 23:15:45 +0000

    new try

--- list.c
+++ list.c
@@ -86,17 +86,35 @@ void pushFront(List * list, void * data) {
   if(list->head!=NULL){
     list->head->prev=x;
   }
-  list->head=x;
+  else{
+    list->head=x;
+  }
+  
   x->prev=NULL;
 }
 
 void pushBack(List * list, void * data) {
+  Node* x=createNode(data);
   list->current = list->tail;
   pushCurrent(list,data);
+  
 }
 
 void pushCurrent(List * list, void * data) {
-  
+  Node* x=createNode(data);
+  if(list->head!=NULL){
+    Node* puntero=list->head;
+    int posicion=0;
+    while((posicion<list->current) && (puntero->next){
+      puntero=puntero->next;
+      posicion++;
+    }
+    x->next=puntero->next;
+    puntero->next=x;
+  }
+  else{
+    list->head=x;
+  }
 }
 
 void * popFront(List * list) {
